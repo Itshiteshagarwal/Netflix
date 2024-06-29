68924c8d746f8b204d6824fb839f511613456db4
tutorials-git <agarwalhitesh551@gmail.com>
2024-06-29 21:17:21 +0530

    another commit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './TitleCards.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom'
 
 const TitleCards = ({ title, category }) => {
-  const [apiData, setApiData] = useState([]);
+
+  const[apiData, setApiData] = useState([]);
   const cardsRef = useRef();
   const options = {
     method: 'GET',
@@ -13,50 +15,35 @@ const TitleCards = ({ title, category }) => {
     }
   };
 
+
+
   const handleWheel = (event) => {
-    if (cardsRef.current) {
-      event.preventDefault();
-      cardsRef.current.scrollLeft += event.deltaY;
-    }
-  };
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options);
-        const data = await response.json();
-        setApiData(data.results);
-      } catch (err) {
-        console.error(err);
-      }
-    };
 
-    fetchData();
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
 
-    if (cardsRef.current) {
-      cardsRef.current.addEventListener('wheel', handleWheel);
-    }
-
-    return () => {
-      if (cardsRef.current) {
-        cardsRef.current.removeEventListener('wheel', handleWheel);
-      }
-    };
-  }, [category]);
-
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, [])
   return (
     <div className='titlecards'>
       <h2>{title ? title : "Popular On Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {apiData.map((card, index) => (
-          <Link to={`/player/${card.id}`} className="card" key={index}>
-            <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt={card.original_title} />
+        {apiData.map((card, index) => {
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
             <p>{card.original_title}</p>
           </Link>
-        ))}
+        })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TitleCards;
+export default TitleCards
